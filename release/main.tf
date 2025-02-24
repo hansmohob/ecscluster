1ae32d5d1f1933bd6cdea54d1bfa4576c4a48eb9
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-02-24 07:12:42 +0000

    easeofuse

--- release/main.tf
+++ release/main.tf
@@ -229,22 +229,6 @@ resource "aws_s3_bucket_policy" "website" {
   })
 }
 
-# Sample index.html file for website
-resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.website.id
-  key          = "index.html"
-  content      = <<EOF
-<html>
-<body>
-<h1>Hello from Terraform!</h1>
-<p>If you see this, your pipeline is working.</p>
-<p>Deployed at: ${timestamp()}</p>
-</body>
-</html>
-EOF
-  content_type = "text/html"
-}
-
 # WAF web ACL to protect CloudFront distribution
 resource "aws_wafv2_web_acl" "website" {
   provider    = aws.us-east-1
