1b869d856a28e9d68ab11b954fe3ca114675427e
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-04-17 19:08:01 +0100

    fdws

--- eks-infrastructure/ecr_repository.tf
+++ eks-infrastructure/ecr_repository.tf
@@ -22,3 +22,33 @@ module "ecr_repo_service4" {
   name        = "${var.prefix_code}-ecr-userservice"
   kms_key_arn = aws_kms_key.eks.arn
 }
+
+# Create SSM parmeter to pass to service-layer git_pipeline
+resource "aws_ssm_parameter" "ecr_repositories" {
+  name        = "${var.prefix_code}-ecr-repositories"
+  description = "ECR Repository URLs"
+  type        = "String"
+  value = jsonencode({
+    frontend = {
+      url = module.ecr_repo_service1.repository_url
+      arn = module.ecr_repo_service1.repository_arn
+    }
+    orderservice = {
+      url = module.ecr_repo_service2.repository_url
+      arn = module.ecr_repo_service2.repository_arn
+    }
+    productapi = {
+      url = module.ecr_repo_service3.repository_url
+      arn = module.ecr_repo_service3.repository_arn
+    }
+    userservice = {
+      url = module.ecr_repo_service4.repository_url
+      arn = module.ecr_repo_service4.repository_arn
+    }
+  })
+
+  tags = {
+    Name         = "${var.prefix_code}-ecr-repos"
+    resourcetype = "compute"
+  }
+}
