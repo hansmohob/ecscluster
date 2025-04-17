6cec8c71302debec272fd116911e23890d0b6096
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-04-17 15:51:51 +0100

    hbtgf

--- eks-infrastructure/versions.tf
+++ eks-infrastructure/versions.tf
@@ -2,16 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.94.1"
+      version = "~> 5.38.0"
     }
   }
-}
 
-# Use Amazon S3 for Terraform backend
-terraform {  
   backend "s3" {  
     key          = "terraform/statefile.tfstate"
     encrypt      = true
-    use_lockfile = true  #S3 native locking
+    use_lockfile = true
   }
 }
