4ef0a55b2b261616c367645108ccd3c0cb7be429
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-04-17 23:57:41 +0100

    hgfhrg

--- eks-infrastructure/ecr_repository.tf
+++ eks-infrastructure/ecr_repository.tf
@@ -1,54 +1,49 @@
 # ECR repositories
-module "ecr_repo_service1" {
+module "ecr_repo_frontend" {
   source      = "./modules/ecr_repository"
   name        = "${var.prefix_code}-ecr-frontend"
   kms_key_arn = aws_kms_key.eks.arn
 }
 
-module "ecr_repo_service2" {
+module "ecr_repo_orderservice" {
   source      = "./modules/ecr_repository"
   name        = "${var.prefix_code}-ecr-orderservice"
   kms_key_arn = aws_kms_key.eks.arn
 }
 
-module "ecr_repo_service3" {
+module "ecr_repo_productapi" {
   source      = "./modules/ecr_repository"
   name        = "${var.prefix_code}-ecr-productapi"
   kms_key_arn = aws_kms_key.eks.arn
 }
 
-module "ecr_repo_service4" {
+module "ecr_repo_userservice" {
   source      = "./modules/ecr_repository"
   name        = "${var.prefix_code}-ecr-userservice"
   kms_key_arn = aws_kms_key.eks.arn
 }
 
-# Create SSM parmeter to pass to service-layer git_pipeline
-resource "aws_ssm_parameter" "ecr_repositories" {
-  name        = "/${var.prefix_code}/ecr/repositories"
-  description = "ECR Repository URLs"
-  type        = "String"
-  value = jsonencode({
-    frontend = {
-      url = module.ecr_repo_service1.repository_url
-      arn = module.ecr_repo_service1.repository_arn
-    }
-    orderservice = {
-      url = module.ecr_repo_service2.repository_url
-      arn = module.ecr_repo_service2.repository_arn
-    }
-    productapi = {
-      url = module.ecr_repo_service3.repository_url
-      arn = module.ecr_repo_service3.repository_arn
-    }
-    userservice = {
-      url = module.ecr_repo_service4.repository_url
-      arn = module.ecr_repo_service4.repository_arn
-    }
-  })
+# Create SSM parmeters to pass to service-layer git_pipeline
+resource "aws_ssm_parameter" "ecr_frontend" {
+  name  = "/${var.prefix_code}/ecr/frontend"
+  type  = "String"
+  value = module.ecr_repo_frontend.repository_url
+}
 
-  tags = {
-    Name         = "${var.prefix_code}-ecr-repos"
-    resourcetype = "compute"
-  }
+resource "aws_ssm_parameter" "ecr_orderservice" {
+  name  = "/${var.prefix_code}/ecr/orderservice"
+  type  = "String"
+  value = module.ecr_repo_orderservice.repository_url
+}
+
+resource "aws_ssm_parameter" "ecr_productapi" {
+  name  = "/${var.prefix_code}/ecr/productapi"
+  type  = "String"
+  value = module.ecr_repo_productapi.repository_url
+}
+
+resource "aws_ssm_parameter" "ecr_userservice" {
+  name  = "/${var.prefix_code}/ecr/userservice"
+  type  = "String"
+  value = module.ecr_repo_userservice.repository_url
 }
