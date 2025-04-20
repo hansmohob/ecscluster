0ea49d3f76ef72ba75b5b440a1979889ab69c442
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-04-20 20:34:32 +0100

    vsdfvsd

--- platform-config/streamliteksops01/deploy.sh
+++ platform-config/streamliteksops01/deploy.sh
@@ -7,7 +7,7 @@ IMAGE_TAG=$(aws ssm get-parameter --name "/msn/ecr/image-tag" --query "Parameter
 OBSERVABILITY_REPO=$(aws ssm get-parameter --name "/msn/ecr/observability" --query "Parameter.Value" --output text)
 
 # Replace placeholder value in deployment file
-sed -i "s|PLACEHOLDER_OBSERVABILITY_IMAGE|${OBSERVABILITY_REPO}:${IMAGE_TAG}|g" k8s/observability/deployment.yaml
+sed -i "s|PLACEHOLDER_OBSERVABILITY_IMAGE|${OBSERVABILITY_REPO}:${IMAGE_TAG}|g" k8s/deployment.yaml
 
 # Apply the kubernetes manifests
 kubectl apply -f k8s/observability/
